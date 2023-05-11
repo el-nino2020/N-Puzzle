759d8fd18ad96841cc14ede062bc383f5b59880b
reisen <null>
2023-05-11 09:32:14 +0800

    init

---
+++ src/test/java/org/example/npuzzle/NpuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.npuzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
